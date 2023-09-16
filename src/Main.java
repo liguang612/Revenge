1caa5a9e8bbe7346059721b59dba95ac8eea9aa6
liguang612 <p.lyquangk.lythedan@gmail.com>
2023-09-16 22:51:24 +0700

    Fix

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import controller.HomepageController;
 import view.Homepage;
 
 class Main {
     public static void main(String[] args) {  
-        new Homepage();
+        Homepage homepage = new Homepage();
+        new HomepageController(homepage);
     }
 }
---
+++ src/controller/HomepageController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.Homepage;
+
+public class HomepageController {
+    private Homepage homepage;
+
+    public HomepageController(Homepage homepage) {
+        this.homepage = homepage;
+    }
+
+    class HomepageListener implements ActionListener {
+        public void actionPerformed(ActionEvent ae) {
+
+        }
+    }
+}
