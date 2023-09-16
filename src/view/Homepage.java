77810243cac5d2c1805849405a947a510305c743
liguang612 <p.lyquangk.lythedan@gmail.com>
2023-09-16 22:07:13 +0700

    Sửa giao diện TitlePanel

--- src/view/Homepage.java
+++ src/view/Homepage.java
@@ -1,11 +1,11 @@
 package view;
 
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 
@@ -16,15 +16,18 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
 public class Homepage {
     private GridBagLayout gb;
     private GridBagConstraints gbc;
+    private JButton editingButton, settingButton;
     private JFrame mainFrame;
-    private JLabel accountLabel;
+    private JLabel accountLabel, directoryLabel;
     private JPanel accountPanel, contentPanel, titlePanel;
     
     public Homepage() {
+        UIManager.put("Button.font", new Font("SegoeUI", Font.BOLD, 16));
         UIManager.put("Label.font", new Font("SegoeUI", Font.PLAIN, 14));
         mainFrame = new JFrame("Phần mềm thi trắc nghiệm");
 
@@ -46,7 +49,7 @@ public class Homepage {
 
     public void account() {
         accountPanel = new JPanel();
-        accountPanel.setBackground(Color.BLUE);
+        accountPanel.setBackground(new Color(0, 159, 229, 255));
         accountPanel.setLayout(new BorderLayout());
 
         accountLabel = new JLabel("   Bùi Anh Quốc    ", new ImageIcon(new ImageIcon(System.getProperty("user.dir") + "\\src\\image\\avatar.jpg").getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH)), 0);
@@ -55,7 +58,7 @@ public class Homepage {
         accountPanel.add(accountLabel, BorderLayout.EAST);
 
         gbc.insets = new Insets(0, 0, 10, 0);
-        gbc.gridx = 0; gbc.gridy = 0; gbc.weightx = 1; gbc.weighty = 50;
+        gbc.gridx = 0; gbc.gridy = 0; gbc.weightx = 1; gbc.weighty = 30;
 
         mainFrame.add(accountPanel, gbc);
     }
@@ -66,27 +69,58 @@ public class Homepage {
         contentPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         gbc.insets = new Insets(0, 10, 10, 10);
-        gbc.gridx = 0; gbc.gridy = 2; gbc.weightx = 1; gbc.weighty = 00;
+        gbc.gridx = 0; gbc.gridy = 2; gbc.weightx = 1; gbc.weighty = 1050;
 
         mainFrame.add(contentPanel, gbc);
     }
 
     public void title() {
-        titlePanel = new JPanel();
-        titlePanel.setBackground(Color.WHITE);
-        titlePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        titlePanel.setLayout(new GridLayout(3, 2, 20, 10));
+        GridBagConstraints _gbc = new GridBagConstraints();
+        JLabel homeLabel = new JLabel(new ImageIcon(System.getProperty("user.dir") + "\\src\\image\\home.png"));
+        JPanel panel1 = new JPanel(new BorderLayout()), panel2 = new JPanel(new BorderLayout()), panel3 = new JPanel(new BorderLayout());
 
-        titlePanel.add(new JLabel("home"));
-        titlePanel.add(new JButton("settings"));
-        titlePanel.add(new JLabel("path"));
-        titlePanel.add(new JLabel());
-        titlePanel.add(new JLabel());
-        titlePanel.add(new JButton("Editing"));
+        directoryLabel = new JLabel("Home");
+
+        editingButton = new JButton("TURN EDITTING ON");
+        editingButton.setBackground(new Color(0, 159, 229, 255));
+        editingButton.setForeground(Color.WHITE);
+        editingButton.setPreferredSize(new Dimension(200, 40));
 
         gbc.insets = new Insets(0, 10, 10, 10);
         gbc.gridx = 0; gbc.gridy = 1; gbc.weightx = 1; gbc.weighty = 200;
 
+        settingButton = new JButton(new ImageIcon(System.getProperty("user.dir") + "\\src\\image\\setting.png"));
+        settingButton.setBackground(Color.WHITE);
+        
+
+        panel1.setBackground(Color.WHITE);
+        panel1.setBorder(new EmptyBorder(20, 20, 20, 20));
+        panel1.setLayout(new BorderLayout());
+        panel1.add(directoryLabel, BorderLayout.SOUTH);
+        panel1.add(homeLabel, BorderLayout.WEST);
+
+        panel2.setBackground(Color.WHITE);
+        panel2.setBorder(new EmptyBorder(10, 10, 10, 20));
+        panel2.add(settingButton);
+
+        panel3.setBackground(Color.WHITE);
+        panel3.setBorder(new EmptyBorder(10, 10, 30, 20));
+        panel3.add(editingButton);
+        
+        titlePanel = new JPanel();
+        titlePanel.setBackground(Color.WHITE);
+        titlePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        titlePanel.setLayout(new GridBagLayout());
+        
+        _gbc.anchor = GridBagConstraints.NORTHWEST; _gbc.gridx = 0; _gbc.gridy = 0; _gbc.weightx = 1; _gbc.weighty = 1; titlePanel.add(panel1, _gbc);
+        _gbc.anchor = GridBagConstraints.NORTHEAST; _gbc.gridx = 1; _gbc.gridy = 0; _gbc.weightx = 1; _gbc.weighty = 1; titlePanel.add(panel2, _gbc);
+        _gbc.anchor = GridBagConstraints.SOUTHWEST; _gbc.gridx = 0; _gbc.gridy = 1; _gbc.weightx = 1; _gbc.weighty = 1; titlePanel.add(new JLabel(), _gbc);
+        _gbc.anchor = GridBagConstraints.SOUTHEAST; _gbc.gridx = 1; _gbc.gridy = 1; _gbc.weightx = 1; _gbc.weighty = 1; titlePanel.add(panel3, _gbc);
+
         mainFrame.add(titlePanel, gbc);
     }
+
+    JPanel getAccountPanel() {return accountPanel;}
+    JPanel getContentPanel() {return contentPanel;}
+    JPanel getTitlePanel() {return titlePanel;}
 }
